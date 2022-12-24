2c06911f53d1a655a703f0d883d958dc65c7992a
Alyx DARENNE <alyx.darenne@deptagency.com>
2022-12-24 13:30:07 +0100

    Add Redux Persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 import employeesReducer from '../features/employees-slice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({ 
+  employees: employeesReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 export const store = configureStore({
-  reducer: {
-    employees: employeesReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk]
 });
+
+export const persistor = persistStore(store)
